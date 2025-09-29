6852d809bd89e86ce87210a0e893d0e6272ac7d3
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:17:30 +0530

    Update dict3.py

--- LAB/dict3.py
+++ LAB/dict3.py
@@ -1,12 +1,13 @@
 # Write a Python program to update a value in a dictionary. 
-
+#create dict
 dict1={1:"utsav",
        2:"kathiriya",
        3:"keshod",
        4:"junagadh",
        5:20,
        6:30}
-
+#dict updatre in key 5 and new value is 60
 dict1.update({5:60})
 
+#print new dict
 print(dict1)
