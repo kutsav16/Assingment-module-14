f4d2b6e842d572b3827bb51c44baf30a83660616
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:27:06 +0530

    Update fun_print.py

--- LAB/fun_print.py
+++ LAB/fun_print.py
@@ -1,9 +1,10 @@
 #Write a Python program to create a function that takes a string as input and prints it. 
-
+#cteate one function
 def print_str(str):
     print("your string is : ",str)
-    
+ #input string   
 str=input("Enter your string : ")
 
 print_str(str)
+
     
