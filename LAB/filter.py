5f97fbfbcd3c7f629efd0a8af136a0c3dd35b437
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:21:52 +0530

    Update filter.py

--- LAB/filter.py
+++ LAB/filter.py
@@ -1,9 +1,10 @@
 #Write a Python program that filters out even numbers using the filter() function. 
-
+# create function 
 def even(num):
     return num%2==0
-
+# create list
 lst=[1,2,3,4,5]
 
 lst2=list(filter(even,lst))
+
 print(lst2)
