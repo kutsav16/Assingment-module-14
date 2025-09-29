31e410e7864f798a4a53c2f888ab4dcba059595d
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:30:51 +0530

    Update gen.py

--- LAB/gen.py
+++ LAB/gen.py
@@ -1,12 +1,16 @@
 #Write a generator function that generates the first 10 even numbers. 
-
+#create function for generater
 def Even():
+    #starting num value from 2
     num=2
     count=0
     while count<10:
         yield num
+        #num value num+=2
         num +=2
+        #count++
         count +=1
         
 for i in Even():
+
     print(i)
