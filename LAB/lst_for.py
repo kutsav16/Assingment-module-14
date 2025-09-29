28eecb2117a04f4a8c529da021938003a6b1835a
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:38:54 +0530

    Update lst_for.py

--- LAB/lst_for.py
+++ LAB/lst_for.py
@@ -1,7 +1,9 @@
 #Write a Python program to iterate over a list using a for loop.  
-
+# create list
 lst_data=[23,"utsav",20.01,True,[1,2,3]]
-
+# i in lst_data value in index
 for i in lst_data:
+    #print i value
     print(i)
+
 
