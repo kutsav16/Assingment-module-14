d2e1d077f8eb48e73ed2bf135c2c4415a8269ff5
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:21:03 +0530

    Update dict4.py

--- LAB/dict4.py
+++ LAB/dict4.py
@@ -1,13 +1,15 @@
 
 
-
+# create two list
 lst=[1,2,3,4]
 lst2=['a','b','c','d']
-
+# create null dict
 dict={}
 
 for i in range(len(lst)):
+    #lst[i] in key and lst2 in value
     dict[lst[i]]=lst2[i]
     
     
+
 print(dict)
