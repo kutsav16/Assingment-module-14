8ae2e877377f7cde39426ac57263aea18a67007b
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:24:34 +0530

    Update fun_cal.py

--- LAB/fun_cal.py
+++ LAB/fun_cal.py
@@ -1,12 +1,13 @@
+#create function for sum
 def sum(num1,num2):
     return num1+num2
-
+#create function for sub
 def sub(num1,num2):
     return num1-num2
-
+#create function for mul
 def mul(num1,num2):
     return num1*num2
-
+#create function for div
 def div(num1,num2):
     return num1/num2
 
@@ -16,8 +17,9 @@ while True:
     print("3. mul : ")
     print("4. div : ")
     print("5. Exit :")
-    
+    # enter your choice (1-5)
     choice=int(input("Enter your choice : "))
+    #enter 2 number
     num1=int(input("enter number 1 :"))
     num2=int(input("Enter number 2 :"))
     match(choice):
@@ -34,4 +36,5 @@ while True:
             print(div(num1,num2))
             
         case 5:
+
             break
