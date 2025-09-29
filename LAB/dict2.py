2029add1954af96921fa8bf410dcdc25195cd353
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:15:46 +0530

    Update dict2.py

--- LAB/dict2.py
+++ LAB/dict2.py
@@ -1,11 +1,12 @@
 #Write a Python program to access values using dictionary keys. 
-
+# create dict and give value
 dict1={1:"utsav",
        2:"kathiriya",
        3:"keshod",
        4:"junagadh",
        5:20,
        6:30}
-
+# i in value 
 for i in dict1.values():
+       #print value
     print(i)
