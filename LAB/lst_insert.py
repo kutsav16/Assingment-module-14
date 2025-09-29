eb7c8185ee99e9c3b0a91142f83099179f4df020
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:40:33 +0530

    Update lst_insert.py

--- LAB/lst_insert.py
+++ LAB/lst_insert.py
@@ -1,7 +1,9 @@
+#create list
 lst_data=[23,"utsav",20.01,True,[1,2,3]]
-
+#insert 2 index in value 40
 lst_data.insert(2,40)
 print(lst_data)
-
+#append value in 30
 lst_data.append(30)
+
 print(lst_data)
