33059081567edf8950005bce615c3361cc4c66ba
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-29 22:13:56 +0530

    Update dict.py

--- LAB/dict.py
+++ LAB/dict.py
@@ -1,11 +1,12 @@
 #Write a Python program to create a dictionary with 6 key-value pairs.
-
+# create dict and give value
 dict1={1:"utsav",
        2:"kathiriya",
        3:"keshod",
        4:"junagadh",
        5:20,
        6:30}
-
+# i in key and j in value
 for i,j in dict1.items():
+       # i in printing key and j in value 
     print(i,j)
