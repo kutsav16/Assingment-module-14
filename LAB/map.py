567f63732c6bfd96f86d89731582b08c63e5d175
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-30 08:10:30 +0530

    Update map.py

--- LAB/map.py
+++ LAB/map.py
@@ -1,9 +1,10 @@
 #Write a Python program to apply the map() function to square a list of numbers. 
-
+#create function
 def seq(num):
     return num*num
-
+#create list
 lst=[1,2,3,4,5]
-
+#using map function
 lst2=list(map(seq,lst))
+
 print(lst2)
