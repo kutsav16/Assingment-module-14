8617ccd159c229dd2a9df1b61276e2a103c80e5b
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-30 08:05:55 +0530

    Update lst_print.py

--- LAB/lst_print.py
+++ LAB/lst_print.py
@@ -1,10 +1,12 @@
 #Write a Python program to create a list of multiple data type elements.
-
+#create list
 lst_data=[23,"utsav",20.01,True,[1,2,3]]
-
+#print list data
 print(lst_data)
+#find type
 print(type(lst_data))
-
+#ptint type of list each element
 for i in lst_data:
     print(type(i))
+
 
