4b9305a52ca1ac8486a666f43d2e2503ff24ffe3
utsav kathirita <utsavkathiriya16@gmail.com>
2025-09-30 08:04:36 +0530

    Update lst_pop.py

--- LAB/lst_pop.py
+++ LAB/lst_pop.py
@@ -1,10 +1,9 @@
-from os import remove
-
-
+#create list
 lst_data=[23,"utsav",20.01,True,[1,2,3]]
 #work on index
 lst_data.pop(2)
 print(lst_data)
 #work on value
 lst_data.remove(23)
+
 print(lst_data)
